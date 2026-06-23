5f47b8f71f3a3e760c03bd148f0c0fa86f94c582
elias-kodehode <elias96.kodehode@gmail.com>
2026-06-23 12:51:55 +0200

    stuff

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    
+    <nav>
+      <div>
+        <Link to="/">MyShop</Link>
+      </div>
+    </nav>
+  );
+}
